b9130d4f6f179068edb29e12c6e0902f9e28ab5d
kavindadilshan <kavindadilshan97@gmail.com>
2024-01-23 00:51:43 +0530

    create color pallet

---
+++ src/const/Colors.js
@@ -0,0 +1,72 @@
+
+export const APP_COLOR_DARK_GREEN = 'green';
+export const APP_COLOR_RED = 'red';
+export const APP_COLOR_GRAY = 'gray';
+
+export const APP_COLOR_WHITE = '#FFFFFF';
+
+export const APP_COLOR_BLACK = '#000000';
+export const APP_COLOR_ALPHA_BLACK = '#00000000';
+export const APP_COLOR_BLACK_2 = '#2C2C2C';
+export const APP_COLOR_BLACK_3 = '#535353';
+export const APP_COLOR_BLACK_4 = '#636363';
+export const APP_COLOR_BLACK_5 = '#3F3F3F';
+export const APP_COLOR_BLACK_6 = '#626262';
+export const APP_COLOR_BLACK_7 = '#393939';
+export const APP_COLOR_LIGHT_BLACK = '#7A7A7A';
+export const APP_COLOR_LIGHT_BLACK_2 = '#717171';
+export const APP_COLOR_LIGHT_BLACK_3 = '#777777';
+export const APP_COLOR_LIGHT_BLACK_4 = '#585858';
+export const APP_COLOR_LIGHT_BLACK_5 = '#454A4E';
+export const APP_COLOR_LIGHT_BLACK_6 = '#454545';
+export const APP_COLOR_LIGHT_BLACK_7 = '#232323';
+export const APP_COLOR_LIGHT_BLACK_8 = '#242424';
+export const APP_COLOR_LIGHT_BLACK_9 = '#7C7C7C';
+export const APP_COLOR_LIGHT_BLACK_10 = '#686868';
+export const APP_COLOR_LIGHT_BLACK_11 = '#5E5E5E';
+export const APP_COLOR_LIGHT_BLACK_12 = '#00000099';
+
+export const APP_COLOR_GRAY_LIGHT = '#676767';
+export const APP_COLOR_GRAY_LIGHT_1 = '#6D6D6D';
+export const APP_COLOR_GRAY_LIGHT_2 = '#9D9D9D';
+export const APP_COLOR_GRAY_LIGHT_3 = '#858585';
+export const APP_COLOR_GRAY_LIGHT_4 = '#AEAEAE';
+export const APP_COLOR_GRAY_LIGHT_5 = '#898989';
+export const APP_COLOR_GRAY_LIGHTEST = '#bdbdbd';
+export const APP_COLOR_LIGHT_GRAY_2 = '#C9C9C9';
+export const APP_COLOR_LIGHT_GRAY_3 = '#C6C6C6';
+export const APP_COLOR_LIGHT_GRAY_4 = '#F5F5F5';
+export const APP_COLOR_GRAY_LIGHTEST_2 = '#D6D6D6';
+export const APP_COLOR_GRAY_LIGHTEST_3 = '#EAEAEA';
+export const APP_COLOR_GRAY_LIGHTEST_4 = '#EBEBEB';
+export const APP_COLOR_GRAY_LIGHTEST_5 = '#E5E5E5';
+export const APP_COLOR_GRAY_LIGHTEST_6 = '#EDEDED';
+export const APP_COLOR_GRAY_LIGHTEST_7 = '#F4F4F4';
+export const APP_COLOR_GRAY_LIGHTEST_8 = '#eeeeee';
+export const APP_COLOR_GRAY_LIGHTEST_9 = '#DCDCDC';
+
+export const APP_COLOR_BLUE = '#3157B9';
+export const APP_COLOR_BLUE_1 = '#2079FF';
+export const APP_COLOR_BLUE_2 = '#007AFF';
+export const APP_COLOR_DARK_BLUE = '#4B6883';
+export const APP_COLOR_LIGHT_BLUE = '#63A6E2';
+export const APP_COLOR_LIGHT_BLUE_1 = '#20BCFF';
+export const APP_COLOR_LIGHTEST_BLUE = '#CBEFFF';
+
+export const APP_COLOR_GREEN = '#5BC559';
+export const APP_COLOR_GREEN_1 = '#3EBB5A';
+export const APP_COLOR__LIGHT_GREEN = '#4BD069';
+export const APP_COLOR__LIGHTEST_GREEN = '#DCF1E1';
+export const APP_COLOR__LIGHTEST_GREEN_1 = '#EBF6EE';
+
+export const APP_COLOR__LIGHT_PINK = '#FFE1CB';
+export const APP_COLOR__LIGHT_PINK_1 = '#F1DCDC';
+
+export const APP_COLOR_ORANGE = '#FFA31A';
+
+export const APP_COLOR_RED_1 = '#DA4646';
+export const APP_COLOR_RED_2 = '#E24949';
+export const APP_COLOR_RED_3 = '#D04B4B';
+export const APP_COLOR_RED_4 = '#E84A4A';
+
+
