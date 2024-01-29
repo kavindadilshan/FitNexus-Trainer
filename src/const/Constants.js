e569fa900f035adb20ef911fbab904afb096c4c9
kavindadilshan <kavindadilshan97@gmail.com>
2024-01-29 23:39:03 +0530

    added Constants file

---
+++ src/const/Constants.js
@@ -0,0 +1,21 @@
+export const BASE_URL = "https://fitzky.ceyentra.lk/v1/"; //Testing server
+// export const BASE_URL = "https://api.fitzkyonline.com/v1"; //Production server -> not used
+// export const BASE_URL = "https://api.fitzky.com/v1"; //Production server
+
+export const RESPONSE_SUCCESS = 200;
+
+export const SPLASH_DELAY = 1000;
+
+export const USER_STATUS_ACTIVE = 'ACTIVE';
+export const USER_STATUS_PENDING = 'PENDING';
+
+export const USER_PROFILE_PIC = require('../assests/user/profile_pic.png');
+
+export const ARE_YOU_SURE_BACK_MSG = 'Are you sure do you really want to go back without saving changes?';
+export const ARE_YOU_SURE_SAVE_MSG = 'Are you sure you want to save these changes?';
+
+
+
+
+export const VERSION = 1;
+
