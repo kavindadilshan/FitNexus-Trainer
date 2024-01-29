c75cf3362ec4d98ad6601b1e7986b402c864de79
kavindadilshan <kavindadilshan97@gmail.com>
2024-01-29 23:32:43 +0530

    apply font file constance

---
+++ src/const/Fonts.js
@@ -0,0 +1,5 @@
+
+export const APP_FONT_REGULAR = 'Gilroy-Regular';
+export const APP_FONT_BOLD = 'Gilroy-Bold';
+export const APP_FONT_SEMIBOLD = 'Gilroy-SemiBold';
+export const APP_FONT_MEDIUM = 'Gilroy-Medium';
